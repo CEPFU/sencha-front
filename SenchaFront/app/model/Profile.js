f238d474bc4b5542e8a453c0b1e705d4113d8302
Simon <kaltsimon@gmail.com>
2015-06-18 14:07:58 +0200

    Added Profile model

---
+++ SenchaFront/app/model/Profile.js
@@ -0,0 +1,15 @@
+Ext.define('SenchaFront.model.Profile', {
+    extend: 'Ext.data.Model',
+    
+    config: {
+        fields: [
+            { name: 'name', type: 'string' }
+        ],
+        associations: [
+            {
+                type: 'hasOne',
+                model: 'SenchaFront.model.Station'
+            }
+        ]
+    }
+});

53d65bf8db99082d47d6d3fafe5b5d47127e9fb0
Simon <kaltsimon@gmail.com>
2015-06-18 13:34:31 +0200

    Added empty profile list controller

---
+++ SenchaFront/app/controller/ProfileListController.js
@@ -0,0 +1,17 @@
+Ext.define('SenchaFront.controller.ProfileListController', {
+    extend: 'Ext.app.Controller',
+    
+    config: {
+        refs: {
+            
+        },
+        control: {
+            
+        }
+    },
+    
+    //called when the Application is launched, remove if not needed
+    launch: function(app) {
+        
+    }
+});

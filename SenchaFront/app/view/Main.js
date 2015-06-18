848f2c440c7138fe375e837f6702ec131fda77d2
Simon <kaltsimon@gmail.com>
2015-06-18 15:13:39 +0200

    Move profilelistview into main view

--- SenchaFront/app/view/Main.js
+++ SenchaFront/app/view/Main.js
@@ -2,15 +2,43 @@ Ext.define('SenchaFront.view.Main', {
     extend: 'Ext.NavigationView',
     xtype: 'main',
     requires: [
-        'SenchaFront.view.ProfileListView'
+        'Ext.plugin.ListSwipeAction',
+        'Ext.dataview.List'
     ],
     config: {
+        ui: 'sencha',
         fullscreen: true,
         items: [
             {
                 title: 'Profiles',
-                xtype: 'profilelistview'
+                xtype: 'list',
+                ui: 'sencha',
+                store: 'profiles',
+                itemTpl: '{name}',
+                scrollable: true,
+
+                plugins: [
+                    {
+                        type: 'listswipeaction',
+                        deleteButton: true,
+                        ui: 'sencha',
+                        syncDelete: true
+                    }
+                ]
             }
-        ]
+        ],
+        navigationBar: {
+            ui: 'sencha',
+            items: [
+                {
+                    xtype: 'spacer'
+                },
+                {
+                    xtype: 'button',
+                    id: 'addProfileButton',
+                    iconCls: 'add'
+                }
+            ]
+        }
     }
 });

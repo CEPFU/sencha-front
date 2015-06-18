d0eac29d8bf913f64c69858296149a88fab3b9ac
Simon <kaltsimon@gmail.com>
2015-06-18 17:24:18 +0200

    Allow editing of profiles

--- SenchaFront/app/controller/ProfileListController.js
+++ SenchaFront/app/controller/ProfileListController.js
@@ -1,20 +1,32 @@
 Ext.define('SenchaFront.controller.ProfileListController', {
     extend: 'Ext.app.Controller',
+
+    requires: [
+        'SenchaFront.form.ProfileForm'
+    ],
     
     config: {
         refs: {
-            addProfileButton: '#addProfileButton'
+            addProfileButton: '#addProfileButton',
+            profileList: '#profileList',
+            navigationView: '#navigationView'
         },
         control: {
             addProfileButton: {
                 tap: 'addProfile'
+            },
+            profileList: {
+                itemtap: 'listItemTapped'
+            },
+            navigationView: {
+                back: 'goBack'
             }
         }
     },
     
     //called when the Application is launched, remove if not needed
     launch: function(app) {
-        
+
     },
 
     getStore: function() {
@@ -28,6 +40,23 @@ Ext.define('SenchaFront.controller.ProfileListController', {
         var store = this.getStore();
         store.add(newRecord);
         store.sync();
-        // this.editProfile(newRecord);
+        this.editProfile(newRecord);
+    },
+
+    listItemTapped: function(view, index, element, record) {
+        this.editProfile(record);
+    },
+
+    editProfile: function(record) {
+        var navView = this.getNavigationView();
+        // Make the profile edit form
+        var form = Ext.create('SenchaFront.form.ProfileForm');
+        form.setRecord(record);
+        navView.push(form);
+        this.getAddProfileButton().hide();
+    },
+
+    goBack: function() {
+        this.getAddProfileButton().show();
     }
 });

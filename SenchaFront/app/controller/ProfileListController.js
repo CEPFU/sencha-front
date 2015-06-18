d5c3e2443e9652573ea599cbdb5973cc282bda0c
Simon <kaltsimon@gmail.com>
2015-06-18 15:09:48 +0200

    Add method for adding new profile

--- SenchaFront/app/controller/ProfileListController.js
+++ SenchaFront/app/controller/ProfileListController.js
@@ -3,15 +3,31 @@ Ext.define('SenchaFront.controller.ProfileListController', {
     
     config: {
         refs: {
-            
+            addProfileButton: '#addProfileButton'
         },
         control: {
-            
+            addProfileButton: {
+                tap: 'addProfile'
+            }
         }
     },
     
     //called when the Application is launched, remove if not needed
     launch: function(app) {
         
+    },
+
+    getStore: function() {
+        return Ext.StoreMgr.get('profiles');
+    },
+
+    addProfile: function() {
+        var newRecord = Ext.create('SenchaFront.model.Profile', {
+            name: 'New Profile'
+        });
+        var store = this.getStore();
+        store.add(newRecord);
+        store.sync();
+        // this.editProfile(newRecord);
     }
 });
